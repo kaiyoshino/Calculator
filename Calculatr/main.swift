f4a90e663d2750e8ae95dc2cb57dde865d09cb26
Kai Yoshino <kaiyosh@uw.edu>
2016-10-04 18:37:42 -0700

    Initial Commit

---
+++ Calculatr/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculatr
+//
+//  Created by Kai Yoshino on 10/4/16.
+//  Copyright © 2016 Kai Yoshino. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
